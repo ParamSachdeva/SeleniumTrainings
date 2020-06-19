bd6154b4874381a0c57920c6adb5afcef6dad6ba
ParamSachdeva <psparamjeet21@gmail.com>
2020-06-19 23:16:14 +0530

    Update BinarytoDecimal.java

--- src/IVPrograms/BinarytoDecimal.java
+++ src/IVPrograms/BinarytoDecimal.java
@@ -1,18 +1,47 @@
-package IVPrograms;
+package JavaPrograms;
+import java.util.Scanner;
 
 public class BinarytoDecimal {
 
 	public static void main(String[] args) {
-		int test = 10111;
-		int temp,decimal = 0,index = 0; 
-		while(test%10 != 0)
-		{			
-			temp = test%10;
-			test = test/10;
-			decimal = decimal + (temp * (2^index));
-			index++;
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int bi = in.nextInt();
+		int index = 0 , temp, de = 0 ;
+
+		while (bi/10 != 0 || bi ==10)
+		{
+			if(bi == 10)
+			{
+				temp = 1;
+				index ++;
+				de = de + temp * (Powerof(2,index));
+				bi = bi/10;
+				break;
+			}
+			else
+			{
+				temp = bi%10;
+				bi = bi/10;
+				de = de + temp * (Powerof(2,index));
+				index ++;
+			}
 		}
-		System.out.println("Decimal is: " + test);
+		System.out.println(de);
+	}
+
+	static int Powerof(int base, int exponent)
+	{
+		int result = 1;
+
+		while (exponent != 0)
+		{
+			result *= base;
+			--exponent;
+		}
+		//System.out.println("Answer = " + result);
+		return result;
 	}
 
 }
